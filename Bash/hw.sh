ca608715ce6d1d911bd5c6b9a39988de24962511
JEFC <eq.emmanuel.137@gmail.com>
2023-04-03 22:53:24 -0600

    - Hello, World! in Bash

---
+++ Bash/hw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#: Title       : hw
+#: Date        : 2023-04-03
+#: Author      : "Emmanuel Flores" <eq.emmanuel@gmail.com>
+#: Version     : 1.0
+#: Description : print Hello, World!
+#: Options     : None
+
+
+printf "%s\n" "Hello, World!"
