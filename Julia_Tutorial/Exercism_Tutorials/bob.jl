b53bc1969e272992619c53fe920ecbdfe30b3ab7
JEFC <eq.emmanuel.137@gmail.com>
2023-04-06 13:51:50 -0600

    - Update of Julia and Python codes.

--- Julia_Tutorial/Exercism_Tutorials/bob.jl
+++ Julia_Tutorial/Exercism_Tutorials/bob.jl
@@ -1,10 +1,20 @@
-function bob(question)
-    if question[end] == "?"
-        return "Sure."
-    elseif question == uppercase(question)
+function is_upper(word)
+    uppercase(word) == word 
+end
+function is_question(word)
+    endswith(word, "?")
+end
+function bob(stimulus)
+    stimulus = strip(stimulus)
+    if is_upper(stimulus) && any(isuppercase, stimulus) && is_question(stimulus)
+        return "Calm down, I know what I'm doing!"
+    elseif isempty(stimulus) 
+        return "Fine. Be that way!"
+    elseif is_upper(stimulus) && any(isuppercase, stimulus)
         return "Whoa, chill out!"
-    elseif 
+    elseif endswith(stimulus, "?") 
+        return "Sure."
+    else
+        return "Whatever."
     end
 end
-
-println(bob("Tom-ay-to, tom-aaaah-to."))
