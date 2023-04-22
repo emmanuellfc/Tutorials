79b822582d66cb48c78eab2bafaf7c8a5f455390
JEFC <eq.emmanuel.137@gmail.com>
2023-04-22 15:06:22 -0600

    - Upload of Chapter Two

---
+++ Julia_Tutorial/Data_Visualization/Chapter_Two.jl
@@ -0,0 +1,44 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9f9b2776-e142-11ed-2a11-5df983a880da
+md"**The Julia Package Ecosystem**"
+
+# ╔═╡ 9befe6de-6793-4ede-ad17-8c3426fe910b
+md"In this chapter, we're going to cover the following main topics:
+
+* Plotting libraries 
+* Understanding the Plots package 
+* Plots' backends 
+* Introducing Makie
+"
+
+# ╔═╡ 2d9d3a91-a9ec-4a1c-9331-c0e9dcbea515
+begin
+	x = range(0, 2pi, length = 100)
+	y = sin.(x)
+end
+
+# ╔═╡ 960eb95d-3360-467c-a533-70b081189a48
+
+
+# ╔═╡ 3a463b9b-2e91-48ee-bec4-f5dacceaf34a
+
+
+# ╔═╡ a602ffad-f7a4-4f49-a0c1-2c674e4d8e7b
+
+
+# ╔═╡ 226083d0-91fb-4f65-8e4d-e24400e269f0
+
+
+# ╔═╡ Cell order:
+# ╟─9f9b2776-e142-11ed-2a11-5df983a880da
+# ╟─9befe6de-6793-4ede-ad17-8c3426fe910b
+# ╠═2d9d3a91-a9ec-4a1c-9331-c0e9dcbea515
+# ╠═960eb95d-3360-467c-a533-70b081189a48
+# ╠═3a463b9b-2e91-48ee-bec4-f5dacceaf34a
+# ╠═a602ffad-f7a4-4f49-a0c1-2c674e4d8e7b
+# ╠═226083d0-91fb-4f65-8e4d-e24400e269f0
