3d31183b9baa3b96b1d776ecce4c9fb6c280452f
JEFC <eq.emmanuel.137@gmail.com>
2023-05-16 17:07:09 -0600

    - Upload of Files Notebook from Think Julia.

---
+++ Julia_Tutorial/Think_Julia/Files.jl
@@ -0,0 +1,72 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ebf6fe76-f43c-11ed-1140-530994084e89
+md"**Files**"
+
+# ╔═╡ 4166e718-cefa-4136-8991-1551b80a74c8
+fout = open("output.txt", "w")
+
+# ╔═╡ dfae2c70-07ba-4d05-b471-0d7e7d9976a8
+line1 = "This here's the wattle, \n";
+
+# ╔═╡ 57a4e73e-dbe7-4efb-bfb7-629a50c4ab1a
+write(fout, line1)
+
+# ╔═╡ 4aa8284c-fdec-4d26-a8a1-e7a6f490bb02
+line2 = "the emblem of our land.\n";
+
+# ╔═╡ 2b282d9f-26af-42e9-be67-e9bfa8f7231f
+write(fout, line2)
+
+# ╔═╡ 2b0b22c0-dc29-4350-8a0d-70b262fe596b
+close(fout)
+
+# ╔═╡ 70769822-4f97-4b87-81fa-0a0a4a60f545
+camels = 42;
+
+# ╔═╡ 2e73f425-f4c5-48b5-b8af-f6e79608c23d
+println(fout, "I have spotted $camels camels.")
+
+# ╔═╡ a93ca0c7-c438-4b0a-871d-99ea5cb4d2d4
+cwd = pwd()
+
+# ╔═╡ a30d5a16-8f14-4545-a0fe-e66179113bec
+ispath("output.txt")
+
+# ╔═╡ c104a220-aa45-441b-8b23-8479c631da3e
+isdir("output.txt")
+
+# ╔═╡ 76e78865-edfe-47ef-ba85-27b1fa8a9634
+readdir(cwd)
+
+# ╔═╡ cc69222c-5cc5-4f15-a6e1-55c1ae8ae016
+function walk(dirname)
+	for name in readdir(dirname)
+		path = joinpath(dirname, name)
+		if isfile(path)
+			println(path)
+		else
+			walk(path)
+		end
+	end
+end
+
+# ╔═╡ Cell order:
+# ╟─ebf6fe76-f43c-11ed-1140-530994084e89
+# ╠═4166e718-cefa-4136-8991-1551b80a74c8
+# ╠═dfae2c70-07ba-4d05-b471-0d7e7d9976a8
+# ╠═57a4e73e-dbe7-4efb-bfb7-629a50c4ab1a
+# ╠═4aa8284c-fdec-4d26-a8a1-e7a6f490bb02
+# ╠═2b282d9f-26af-42e9-be67-e9bfa8f7231f
+# ╠═2b0b22c0-dc29-4350-8a0d-70b262fe596b
+# ╠═70769822-4f97-4b87-81fa-0a0a4a60f545
+# ╠═2e73f425-f4c5-48b5-b8af-f6e79608c23d
+# ╠═a93ca0c7-c438-4b0a-871d-99ea5cb4d2d4
+# ╠═a30d5a16-8f14-4545-a0fe-e66179113bec
+# ╠═c104a220-aa45-441b-8b23-8479c631da3e
+# ╠═76e78865-edfe-47ef-ba85-27b1fa8a9634
+# ╠═cc69222c-5cc5-4f15-a6e1-55c1ae8ae016
--- Julia_Tutorial/Think_Julia/Tuples.jl
+++ Julia_Tutorial/Think_Julia/Tuples.jl
@@ -75,18 +75,136 @@ function minmax(t)
 end
 
 # ╔═╡ ce119f1d-40e0-4b15-b954-1cd3ae6a74d8
-
+md"Functions can take a variable number or arguments. A parameter name that ends with ... gathers arguments into a tuple."
 
 # ╔═╡ 4e141804-1c79-46e0-9d0e-28bfa358f70e
-
+function printall(args...)
+	println(args)
+end
 
 # ╔═╡ 38082503-eea5-4a88-ac06-fa9feda3285f
+printall(1, 2.0, '3')
+
+# ╔═╡ 2294c8ce-2d40-45ea-91b0-365c55f338d3
+function sumall(s)
+	sum(s)
+end
+
+# ╔═╡ 1becfd44-b392-4426-838d-31af856dbe61
+begin
+	s = (1, 2, 3, 4);
+	sumall(s)
+	#sum(s)
+end
+
+# ╔═╡ e81c8fb1-d816-4623-9ba2-2b83b514ffba
+md"**Arrrays and tuples**
+
+zip is a built-in function that takes two or more sequences and returns a collection of tuples where each tuple contains one element from each sequence."
+
+# ╔═╡ ca457df9-950f-4290-bbb0-ccccfdd5d73c
+begin
+	r1 = "abc"
+	s1 = [1, 2, 3]
+	zip(r1, s1)
+	for pair in zip(r1, s1)
+		println(pair)
+	end
+end
+
+# ╔═╡ 7a683d6a-5b78-46da-8241-ba1f02c1e4ea
+md"A zip object is kind of an iterator, which is any object that iterates through a sequence."
+
+# ╔═╡ 5dd2288b-f601-4dd5-bda1-669ebc676f6c
+collect(zip(r1, s1))
+
+# ╔═╡ 9518cb68-5905-4f63-9802-f1f0eac5937d
+collect(zip("Anne", "Elk"))
+
+# ╔═╡ eee67d63-7f50-4de4-9b40-1949f7de24cf
+begin
+	tn = [('a', 1),('b', 2),('c', 3)]
+	for (letter, number) in tn
+		println(number, " ", letter)
+	end
+end
+
+# ╔═╡ 658b5cf8-0b39-4bfe-89e9-58af51d3f3ec
+md"If you combine zip, for and typle assignments, you get a useful idiom for traversing two(or more) sequences at the same time."
+
+# ╔═╡ 22afdcda-77a6-4146-b500-8ee4fb0b2b89
+function hasmatch(m1, m2)
+	for (x, y) in zip(m1, m2)
+		if x == y
+			return true
+		end
+	end
+	false
+end
+
+# ╔═╡ 9b8cffc6-8362-4958-ac27-6983c53da812
+md"If you need to traverse the elements of a sequence and their indices, you can use the built-in function enumerate:"
+
+# ╔═╡ e9272c4f-b458-41de-9053-05acd2f7c73d
+for (index, element) in enumerate("abc")
+	println(index, " ", element)
+end
+
+# ╔═╡ 53c95e5e-ae8c-45c6-9e34-d5f7d9253084
+md"**Dictionaries and tuples**"
+
+# ╔═╡ a3cb3765-6019-4703-9e18-bbaa8fec11f3
+begin
+	d = Dict('a'=>1, 'b'=> 2, 'c'=>3)
+	for (key, value) in d
+		println(key, " ", value)
+	end
+end
+
+# ╔═╡ cd7360c7-33d0-4253-bbed-f985141eee8b
+md"Going in the other direction, you can use an array of tuples to initialize a new dicionary."
+
+# ╔═╡ b6c7813c-9871-48e1-b346-5930af870bb8
+begin
+	nt = [('a', 1), ('b', 2), ('c', 3)]
+	nd = Dict(nt)
+end
+
+# ╔═╡ de49609a-36cd-4d63-8bf9-b0a22ca37b35
+md"Combining Dict with zip, yields a concise way to create a dictionary:"
+
+# ╔═╡ 235623c6-5726-49e7-93fc-b724b84601f1
+z = Dict(zip("abc", 1:3))
+
+# ╔═╡ 825770aa-4b48-433c-bf83-1eb4cf423e88
+md"Arrays, dictionaries and tuples are examples of data structures; in this lecture we are starting to see compund data structures, like arrays of tuples, or dictionaries that contain tuples as keys and arrays as values. Compund data structures are useful, but they are prone to what I call *shape errors.*"
+
+# ╔═╡ d7aaa85c-4d5e-417d-b843-1f6be2abe908
+md"**Glossary**
+
+* tuple: An immutable sequence of elements where every element can have its own type.
+
+* tuple assignment: An assignment with a sequence on the right side and a tuple of variables on the left. The right side is evaluated and then its elements are assigned to the variables on the left.
+
+* gather: The operation of assembling a variable-length argument tuple.
+
+* scatter: The operation of treating a sequence as a list of arguments.
+
+* zip object: The result of calling a built-in function zip, an object that iterates through a sequence of tuples.
+
+* iterator: An object that can iterate through a sequence, but that does not provide array operators and functions.
+
+* data structure: A collection of related values, often organized in arrays, dictionaries, tuples, etc.
+
+* shape error: An error caused because a value has the wrong shape; that is, the wrong type or size."
+
+# ╔═╡ f65df7db-e2c9-4dc4-b600-98e319cb61d8
 
 
-# ╔═╡ 3a0ae340-3018-4d16-a071-bae09e6476e3
+# ╔═╡ 5f5f6d34-257c-40e5-96c3-5813eb55a0ec
 
 
-# ╔═╡ a10dc257-6344-46fe-b8fc-ae58d1ad7f9c
+# ╔═╡ c9e64daf-a3aa-46fd-9bef-72c31a392b3b
 
 
 # ╔═╡ Cell order:
@@ -107,8 +225,29 @@ end
 # ╠═0b2b80ee-dae1-4067-8917-f4491213df45
 # ╟─4fb1f7cc-1044-4870-8701-1825edcaf564
 # ╠═431d3e16-a858-4109-8708-b4b5e129a36a
-# ╠═ce119f1d-40e0-4b15-b954-1cd3ae6a74d8
+# ╟─ce119f1d-40e0-4b15-b954-1cd3ae6a74d8
 # ╠═4e141804-1c79-46e0-9d0e-28bfa358f70e
 # ╠═38082503-eea5-4a88-ac06-fa9feda3285f
-# ╠═3a0ae340-3018-4d16-a071-bae09e6476e3
-# ╠═a10dc257-6344-46fe-b8fc-ae58d1ad7f9c
+# ╠═2294c8ce-2d40-45ea-91b0-365c55f338d3
+# ╠═1becfd44-b392-4426-838d-31af856dbe61
+# ╟─e81c8fb1-d816-4623-9ba2-2b83b514ffba
+# ╠═ca457df9-950f-4290-bbb0-ccccfdd5d73c
+# ╟─7a683d6a-5b78-46da-8241-ba1f02c1e4ea
+# ╠═5dd2288b-f601-4dd5-bda1-669ebc676f6c
+# ╠═9518cb68-5905-4f63-9802-f1f0eac5937d
+# ╠═eee67d63-7f50-4de4-9b40-1949f7de24cf
+# ╟─658b5cf8-0b39-4bfe-89e9-58af51d3f3ec
+# ╠═22afdcda-77a6-4146-b500-8ee4fb0b2b89
+# ╟─9b8cffc6-8362-4958-ac27-6983c53da812
+# ╠═e9272c4f-b458-41de-9053-05acd2f7c73d
+# ╟─53c95e5e-ae8c-45c6-9e34-d5f7d9253084
+# ╠═a3cb3765-6019-4703-9e18-bbaa8fec11f3
+# ╟─cd7360c7-33d0-4253-bbed-f985141eee8b
+# ╠═b6c7813c-9871-48e1-b346-5930af870bb8
+# ╟─de49609a-36cd-4d63-8bf9-b0a22ca37b35
+# ╠═235623c6-5726-49e7-93fc-b724b84601f1
+# ╟─825770aa-4b48-433c-bf83-1eb4cf423e88
+# ╟─d7aaa85c-4d5e-417d-b843-1f6be2abe908
+# ╠═f65df7db-e2c9-4dc4-b600-98e319cb61d8
+# ╠═5f5f6d34-257c-40e5-96c3-5813eb55a0ec
+# ╠═c9e64daf-a3aa-46fd-9bef-72c31a392b3b
